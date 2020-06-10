2d087b765b8101ea52dd42dc9ef36d932e9fd273
root <vitor@vbranco.fr>
2020-06-10 17:44:47 +0200

    feat: correction -1 et proprete du code

--- ASM/src/Grace.s
+++ ASM/src/Grace.s
@@ -1,13 +1,17 @@
-%define STRING "%%define STRING %2$c%3$s%2$c%1$c%%define FILE %2$cGrace_kid.s%2$c%1$c%%macro FD 0%1$cextern fopen, fprintf, fclose, exit%1$csection .data%1$cfile_name: db FILE,0%1$cfile_mode: db %2$cw%2$c,0%1$cs: db STRING,0%1$csection .text%1$cglobal _start%1$c_start:%1$cmov rdi, file_name%1$cmov rsi, file_mode%1$ccall fopen%1$cmov rbx, rax%1$ccmp rbx, 0%1$cje quit_error%1$cmov rdi, rbx%1$cmov rsi, s%1$cmov rdx, 10%1$cmov rcx, 34%1$cmov r8, s%1$ccall fprintf%1$cmov rdi, rbx%1$ccall fclose%1$cjmp quit%1$cquit:%1$ccall exit%1$cquit_error:%1$cmov rdi, 1%1$ccall exit%1$c%%endmacro%1$c;Show time%1$cFD"
+%define STRING "%%define STRING %2$c%3$s%2$c%1$c%%define FILE %2$cGrace_kid.s%2$c%1$c%1$c%%macro FD 0%1$cextern fopen, fprintf, fclose, exit%1$c%1$csection .data%1$cfile_name: db FILE,0%1$cfile_mode: db %2$cw%2$c,0%1$cs: db STRING,0%1$c%1$csection .text%1$cglobal _start%1$c%1$c_start:%1$cmov rdi, file_name%1$cmov rsi, file_mode%1$ccall fopen%1$cmov rbx, rax%1$ccmp rbx, 0%1$cje quit_error%1$cmov rdi, rbx%1$cmov rsi, s%1$cmov rdx, 10%1$cmov rcx, 34%1$cmov r8, s%1$ccall fprintf%1$cmov rdi, rbx%1$ccall fclose%1$cjmp quit%1$c%1$cquit:%1$ccall exit%1$c%1$cquit_error:%1$cmov rdi, 1%1$ccall exit%1$c%%endmacro%1$c;Show time%1$cFD"
 %define FILE "Grace_kid.s"
+
 %macro FD 0
 extern fopen, fprintf, fclose, exit
+
 section .data
 file_name: db FILE,0
 file_mode: db "w",0
 s: db STRING,0
+
 section .text
 global _start
+
 _start:
 mov rdi, file_name
 mov rsi, file_mode
@@ -24,8 +28,10 @@ call fprintf
 mov rdi, rbx
 call fclose
 jmp quit
+
 quit:
 call exit
+
 quit_error:
 mov rdi, 1
 call exit
--- ASM/src/Sully.s
+++ ASM/src/Sully.s
@@ -1,20 +1,25 @@
-%define STRING "%%define STRING %2$c%3$s%2$c%1$cextern fopen, fprintf, fclose, exit, system, sprintf%1$csection .data%1$cfilemode: db %2$cw%2$c,0%1$cs: db STRING,0%1$ctmp_compile: db %2$cnasm -f elf64 %%1$s.s && clang -Wall -Werror -Wextra -no-pie %%1$s.o -o %%1$s%2$c,0%1$ctmp_execute: db %2$c./%%1$s%2$c,0%1$ctmp_filename: db %2$cSully_%%1$d%2$c,0%1$ctmp_source: db %2$c%%1$s.s%2$c,0%1$ci: db %4$d%1$csection .bss%1$ccompile: resb 1000%1$cexecute: resb 1000%1$cfilename: resb 100%1$csource: resb 1000%1$csection .text%1$cglobal main%1$cft_compile_and_execute:%1$cmov rdi, compile%1$cmov rsi, tmp_compile%1$cmov rdx, filename%1$cmov rax, 0%1$ccall sprintf%1$cmov rdi, compile%1$cmov rax, 0%1$ccall system%1$cmov rdi, execute%1$cmov rsi, tmp_execute%1$cmov rdx, filename%1$cmov rax, 0%1$ccall sprintf%1$cmov rdi, execute%1$cmov rax, 0%1$ccall system%1$cret%1$cft_file:%1$cmov rdi, source%1$cmov rsi, filemode%1$ccall fopen%1$cmov rbx, rax%1$ccmp rbx, 0%1$cje ft_error_quit%1$cmov rdi, rbx%1$cmov rsi, s%1$cmov rdx, 10%1$cmov rcx, 34%1$cmov r8, s%1$cmov r9, [rsp + 8]%1$cmov rax, 0%1$ccall fprintf%1$cmov rdi, rbx%1$ccall fclose%1$cret%1$cft_source:%1$cmov rdi, source%1$cmov rsi, tmp_source%1$cmov rdx, filename%1$cmov rax, 0%1$ccall sprintf%1$cret%1$cft_filename:%1$cmov rdi, filename%1$cmov rsi, tmp_filename%1$cmov rdx, [rsp + 8]%1$cdec rdx%1$cmov [rsp + 8], rdx%1$cmov rax, 0%1$ccall sprintf%1$cret%1$cft_error_quit:%1$cmov rdi, 1%1$ccall exit%1$cquit:%1$cmov rdi, 0%1$ccall exit%1$cmain:%1$cmov rax, [i]%1$ccmp rax, 0%1$cje quit%1$c%%ifdef FIRST%1$cinc rax%1$c%%endif%1$cpush rax%1$ccall ft_filename%1$ccall ft_source%1$ccall ft_file%1$cpop rax%1$ccall ft_compile_and_execute%1$cmov rbx, 0%1$cmov rax, 0%1$cret"
+%define STRING "%%define STRING %2$c%3$s%2$c%1$c%1$cextern fopen, fprintf, fclose, exit, system, sprintf%1$c%1$csection .data%1$cfilemode: db %2$cw%2$c,0%1$cs: db STRING,0%1$ctmp_compile: db %2$cnasm -f elf64 %%1$s.s && clang -Wall -Werror -Wextra -no-pie %%1$s.o -o %%1$s && rm -f %%1$s.o%2$c,0%1$ctmp_execute: db %2$c./%%1$s%2$c,0%1$ctmp_filename: db %2$cSully_%%1$d%2$c,0%1$ctmp_source: db %2$c%%1$s.s%2$c,0%1$ci: db %4$d%1$c%1$csection .bss%1$ccompile: resb 1000%1$cexecute: resb 1000%1$cfilename: resb 100%1$csource: resb 1000%1$c%1$csection .text%1$cglobal main%1$c%1$cft_compile_and_execute:%1$cmov rdi, compile%1$cmov rsi, tmp_compile%1$cmov rdx, filename%1$cmov rax, 0%1$ccall sprintf%1$cmov rdi, compile%1$cmov rax, 0%1$ccall system%1$cmov rdi, execute%1$cmov rsi, tmp_execute%1$cmov rdx, filename%1$cmov rax, 0%1$ccall sprintf%1$cmov rdi, execute%1$cmov rax, 0%1$ccall system%1$cret%1$c%1$cft_file:%1$cmov rdi, source%1$cmov rsi, filemode%1$ccall fopen%1$cmov rbx, rax%1$ccmp rbx, 0%1$cje ft_error_quit%1$cmov rdi, rbx%1$cmov rsi, s%1$cmov rdx, 10%1$cmov rcx, 34%1$cmov r8, s%1$cmov r9, [rsp + 8]%1$cmov rax, 0%1$ccall fprintf%1$cmov rdi, rbx%1$ccall fclose%1$cret%1$c%1$cft_source:%1$cmov rdi, source%1$cmov rsi, tmp_source%1$cmov rdx, filename%1$cmov rax, 0%1$ccall sprintf%1$cret%1$c%1$cft_filename:%1$cmov rdi, filename%1$cmov rsi, tmp_filename%1$cmov rdx, [rsp + 8]%1$cdec rdx%1$cmov [rsp + 8], rdx%1$cmov rax, 0%1$ccall sprintf%1$cret%1$c%1$cft_error_quit:%1$cmov rdi, 1%1$ccall exit%1$c%1$cquit:%1$cmov rdi, 0%1$ccall exit%1$c%1$cmain:%1$cmov rax, [i]%1$ccmp rax, 0%1$cjle quit%1$c%%ifdef FIRST%1$cinc rax%1$c%%endif%1$cpush rax%1$ccall ft_filename%1$ccall ft_source%1$ccall ft_file%1$cpop rax%1$ccall ft_compile_and_execute%1$cmov rbx, 0%1$cmov rax, 0%1$cret"
+
 extern fopen, fprintf, fclose, exit, system, sprintf
+
 section .data
 filemode: db "w",0
 s: db STRING,0
-tmp_compile: db "nasm -f elf64 %1$s.s && clang -Wall -Werror -Wextra -no-pie %1$s.o -o %1$s",0
+tmp_compile: db "nasm -f elf64 %1$s.s && clang -Wall -Werror -Wextra -no-pie %1$s.o -o %1$s && rm -f %1$s.o",0
 tmp_execute: db "./%1$s",0
 tmp_filename: db "Sully_%1$d",0
 tmp_source: db "%1$s.s",0
 i: db 5
+
 section .bss
 compile: resb 1000
 execute: resb 1000
 filename: resb 100
 source: resb 1000
+
 section .text
 global main
+
 ft_compile_and_execute:
 mov rdi, compile
 mov rsi, tmp_compile
@@ -33,6 +38,7 @@ mov rdi, execute
 mov rax, 0
 call system
 ret
+
 ft_file:
 mov rdi, source
 mov rsi, filemode
@@ -51,6 +57,7 @@ call fprintf
 mov rdi, rbx
 call fclose
 ret
+
 ft_source:
 mov rdi, source
 mov rsi, tmp_source
@@ -58,6 +65,7 @@ mov rdx, filename
 mov rax, 0
 call sprintf
 ret
+
 ft_filename:
 mov rdi, filename
 mov rsi, tmp_filename
@@ -67,16 +75,19 @@ mov [rsp + 8], rdx
 mov rax, 0
 call sprintf
 ret
+
 ft_error_quit:
 mov rdi, 1
 call exit
+
 quit:
 mov rdi, 0
 call exit
+
 main:
 mov rax, [i]
 cmp rax, 0
-je quit
+jle quit
 %ifdef FIRST
 inc rax
 %endif
